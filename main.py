9f40ed8d0041e854509a7c8f41b67a69c89ae586
Ronnie Joshua <ronniejoshua@ron-macbookpro-2018.local>
2019-11-28 07:04:15 +0200

    Flask Application - For Google App Engine.

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    author = "Ronnie Joshua"
+    return render_template('index.html', author=author)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
